bb4ac41e284858441ca73a546aa0082f3cf4b215
WiOrca <wiorcaman@gmail.com>
2020-08-25 17:31:00 -0400

    Fix typo in image

--- app-startup.sh
+++ app-startup.sh
@@ -1,11 +1,4 @@
-#1 /bin/bash
-
-# Here is where you run the secure service you want to run.  Do not exit.  Run in foreground.
-# Overwrite this file in your docker image
-
-echo "IP information"
-
-ip addr
+#! /bin/bash
 
 echo "Starting qBittorrent Service"
 
